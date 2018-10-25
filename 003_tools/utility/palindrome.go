6dcb6041aca71dd54db2ce70ec2c5cd6a034460f
nirav thakkar <thakkarnirav@gmail.com>
2018-10-25 09:06:26 -0500

    no message

--- 003_tools/utility/palindrome.go
+++ 003_tools/utility/palindrome.go
@@ -1,1 +1,13 @@
 package utility
+
+func isPalindrome(s string) bool {
+
+	r := []rune(s)
+	for i := 0; i < len(s)/2; i++ {
+		if r[i] != r[len(s)-(i+1)] {
+			return false
+		}
+	}
+	return true
+}
+
--- 003_tools/utility/palindrome_test.go
+++ 003_tools/utility/palindrome_test.go
@@ -1,1 +1,31 @@
 package utility
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIsPalindrome(t *testing.T) {
+
+	testPalindromeVars := []struct {
+		word   string
+		result bool
+	}{
+		{"abc", false},
+		{"abcdcba", true},
+		{"abcd", false},
+		{"abcddcba", true},
+	}
+	for _, testPalindromeVar := range testPalindromeVars {
+		t.Run(testPalindromeVar.word, func(t *testing.T) {
+
+			result := isPalindrome(testPalindromeVar.word)
+
+			if result != testPalindromeVar.result {
+				t.Errorf("%s : want %v result %v", testPalindromeVar.word, result, testPalindromeVar.result)
+			}else {
+				fmt.Print(testPalindromeVar.word, " is palindrome")
+			}
+		})
+	}
+}
